ace0d0ab6449a98713eb0338b9737a1f74ad59ed
anger issues <skuenemy@gmail.com>
2025-02-05 17:46:48 +0300

    second release as a very casual game developer on pure C

--- game_example/build/CMakeFiles/my_program.dir/main.c.o.d
+++ game_example/build/CMakeFiles/my_program.dir/main.c.o.d
@@ -1,5 +1,5 @@
 CMakeFiles/my_program.dir/main.c.o: \
-  /Users/aleksandrsuslin/desktop/game_example/main.c \
+  /Users/aleksandrsuslin/Desktop/tanks-2d/game_example/main.c \
   /opt/homebrew/Cellar/raylib/5.5/include/raylib.h \
   /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/15.0.0/include/stdarg.h \
   /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/15.0.0/include/stdbool.h \
--- game_example/build/cmake_install.cmake
+++ game_example/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/aleksandrsuslin/desktop/game_example
+# Install script for directory: /Users/aleksandrsuslin/Desktop/tanks-2d/game_example
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,7 +40,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/Users/aleksandrsuslin/desktop/game_example/build/install_local_manifest.txt"
+  file(WRITE "/Users/aleksandrsuslin/Desktop/tanks-2d/game_example/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -56,6 +56,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/Users/aleksandrsuslin/desktop/game_example/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/Users/aleksandrsuslin/Desktop/tanks-2d/game_example/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
